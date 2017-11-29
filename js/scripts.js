0fe47541cc7dee463abf1f4f86eb448b9b3a000e
Paul Kotok and Zach Evans <paul@>
2017-11-29 11:16:59 -0800

    Copy other.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,50 @@
-var add = function (number1, number2, number3) {
-return (number1 + number2) *number3;
+// Back-end logic:
+
+var add = function(number1, number2) {
+	return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number here."));
-var number2 = parseInt(prompt("Enter another number here"));
-var number3 = parseInt(prompt("Enter your favorite number here"));
+var subtract = function(number1, number2) {
+  return number1 - number2;
+}
 
-alert(add(number1,number2, number3));
+var multiply = function(number1, number2) {
+  return number1 * number2;
+}
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+}
+
+//Below this line is front-end logic:
+
+$(document).ready(function() {
+	$("form#add").submit(function(event) {
+  event.preventDefault();
+	var number1 = parseInt($("#add1").val());
+	var number2 = parseInt($("#add2").val());
+	var addresult = add(number1, number2);
+	$("#addoutput").text(addresult);
+ 	});
+	$("form#subtract").submit(function(event) {
+  event.preventDefault();
+	var number1 = parseInt($("#subtract1").val());
+	var number2 = parseInt($("#subtract2").val());
+	var subtractresult = subtract(number1, number2);
+	$("#subtractoutput").text(subtractresult);
+ 	});
+ $("form#multiply").submit(function(event) {
+ event.preventDefault();
+ var number1 = parseInt($("#multiply1").val());
+ var number2 = parseInt($("#multiply2").val());
+ var multiplyresult = multiply(number1, number2);
+ $("#multiplyoutput").text(multiplyresult);
+	});
+ $("form#divide").submit(function(event) {
+ event.preventDefault();
+ var number1 = parseInt($("#divide1").val());
+ var number2 = parseInt($("#divide2").val());
+ var divideresult = divide(number1, number2);
+ $("#divideoutput").text(divideresult);
+	});
+});
